751bf10200f10dd3421cd23e391ff1a0059a90c6
unknown <trangiahao498@gmail.com>
2024-11-01 15:40:44 +0700

    register handle, authorization on url access

--- routes/RegisterRouter.js
+++ routes/RegisterRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const RegisterController = require("../controllers/RegisterController");
+const checkLogin = require("../middlewares/CheckLogin");
+
+router.use(checkLogin);
 
 // Route GET - Trang đăng ký
 router.get("/", RegisterController.showRegisterPage);
